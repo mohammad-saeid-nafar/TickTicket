f4fc1bd25167fbfd4ae192deaab063c8771dd734
ericchehata <eric.chehata@mail.mcgill.ca>
2022-10-14 10:29:47 -0400

    Initial commit of the backend application

---
+++ TickTicket-Backend/src/main/java/tickticket/TickTicketApplication.java
@@ -0,0 +1,20 @@
+package tickticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TickTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickTicketApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+}
