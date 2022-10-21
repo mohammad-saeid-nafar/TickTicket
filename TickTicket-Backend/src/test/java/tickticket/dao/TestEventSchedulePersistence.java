fbe29294cb54d98c58490cb13097961d86cb5e39
Abd-El-Aziz Zayed <azayed01@gmail.com>
2022-10-20 20:01:19 -0400

    Add test profile

--- TickTicket-Backend/src/test/java/tickticket/dao/TestEventSchedulePersistence.java
+++ TickTicket-Backend/src/test/java/tickticket/dao/TestEventSchedulePersistence.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import tickticket.model.*;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TestEventSchedulePersistence {
 
     @Autowired
