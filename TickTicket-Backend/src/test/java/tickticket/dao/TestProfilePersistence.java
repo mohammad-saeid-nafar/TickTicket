7801566309da6fa9743be6033847c900f4f43975
Abd-El-Aziz Zayed <azayed01@gmail.com>
2022-10-20 20:01:19 -0400

    Add test profile

--- TickTicket-Backend/src/test/java/tickticket/dao/TestProfilePersistence.java
+++ TickTicket-Backend/src/test/java/tickticket/dao/TestProfilePersistence.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import tickticket.model.EventType;
@@ -23,6 +24,7 @@ import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TestProfilePersistence {
 
 	@Autowired
