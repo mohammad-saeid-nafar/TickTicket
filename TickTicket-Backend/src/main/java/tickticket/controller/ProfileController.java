f2d24ab07c200b5ee79b7770680fab7fc212871b
ericchehata <eric.chehata@mail.mcgill.ca>
2022-11-10 19:45:48 -0500

    Added @CrossOrigin tag to controller classes

--- TickTicket-Backend/src/main/java/tickticket/controller/ProfileController.java
+++ TickTicket-Backend/src/main/java/tickticket/controller/ProfileController.java
@@ -6,18 +6,13 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import tickticket.dto.ProfileDTO;
 import tickticket.model.Profile;
 import tickticket.service.ProfileService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/profiles")
