5429581b960cbcabe338c6bcd71459898e5b9bc2
faditawfik21 <fadi.tawfikbeshay@mail.mcgill.ca>
2022-11-13 11:55:19 -0500

    Event Type Selection from Organization Page

--- TickTicket-Frontend/src/pages/CreateEvent.js
+++ TickTicket-Frontend/src/pages/CreateEvent.js
@@ -1,4 +1,46 @@
+import * as React from 'react';
+import axios from "axios";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 const CreateEvent = () => {
-  return <h1>Create Event</h1>;
+  const [eventTypes, setEventTypes] = React.useState([]);
+  const [alignment, setAlignment] = React.useState('web');
+  const [eventType, setEventType] = React.useState("");
+
+  React.useEffect(() => {
+    axios.get("event-types").then((res) => {
+        setEventTypes(res.data);
+    });
+  }, []);
+
+  const handleChange = (
+    event,
+    newAlignment,
+  ) => {
+    setAlignment(newAlignment);
+    setEventType(event.target.value);
+    // Todo remove console log line when eventType gets used
+    console.log(eventType);
+  };
+
+  return(
+    <>
+      <h1>Create Event</h1>
+      <h2>1. Select Event Type</h2>
+      <ToggleButtonGroup
+        color="primary"
+        value={alignment}
+        exclusive
+        onChange={handleChange}
+      >
+        {eventTypes.map((eventType) => {
+          return (
+            <ToggleButton value={eventType.name}>{eventType.name}</ToggleButton>
+          );
+        })}
+      </ToggleButtonGroup>
+    </>
+  );
 };
 export default CreateEvent;
