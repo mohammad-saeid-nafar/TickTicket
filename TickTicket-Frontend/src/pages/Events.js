3d81dad28e5ea079e5d9e50ccde0e5b3fb4585e5
RobertApra <robert.aprahamian@mail.mcgill.ca>
2022-11-13 19:50:52 -0500

    backend ID013

--- TickTicket-Frontend/src/pages/Events.js
+++ TickTicket-Frontend/src/pages/Events.js
@@ -1,4 +1,34 @@
-const Events = () => {
-  return <h1>Events</h1>;
+// const Events = () => {
+//   return <h1>Events</h1>;
+// };
+// export default Events;
+
+import { Container, Stack } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import EventCard from "../components/EventCard";
+
+const Home = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get("events").then((res) => {
+      setEvents(res.data);
+    });
+  }, []);
+
+  return (
+    <Container
+      sx={{
+        paddingTop: "5%",
+      }}
+    >
+      <Stack spacing={2}>
+        {events.map((event) => {
+          return <EventCard key={event.id} event={event} />;
+        })}
+      </Stack>
+    </Container>
+  );
 };
-export default Events;
+export default Home;
