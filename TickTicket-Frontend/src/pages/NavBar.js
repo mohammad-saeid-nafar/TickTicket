9bfa8807737eb8b5b07c64651ee10aad92dc475a
ericchehata <eric.chehata@mail.mcgill.ca>
2022-11-26 23:26:42 -0500

    Small NavBar fix

--- TickTicket-Frontend/src/pages/NavBar.js
+++ TickTicket-Frontend/src/pages/NavBar.js
@@ -135,7 +135,7 @@ function NavBar() {
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
